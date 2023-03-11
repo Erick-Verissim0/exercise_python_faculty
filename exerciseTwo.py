17912b6c6cfb0896061570f04ea1d30349fde41e
Erick <estudodoerick@gmail.com>
2023-03-11 01:43:28 -0300

    convert_string_in_number

---
+++ exerciseTwo.py
@@ -0,0 +1,7 @@
+numbers = 0
+
+finallyNumber = input('Plis, select or number: ')
+
+while numbers < int(finallyNumber): # o int serve para converter uma string para int
+  print(numbers)
+  numbers += 1
