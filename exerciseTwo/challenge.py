d657610f8a25151aee5f18831b9df15217ee7730
Erick <estudodoerick@gmail.com>
2023-03-11 22:59:51 -0300

    finalized_exercize_Two

---
+++ exerciseTwo/challenge.py
@@ -0,0 +1,12 @@
+print('||||||||||||||||||||||||||||||||||')
+productValue = float(input('Qual o valor do produto? '))
+print('||||||||||||||||||||||||||||||||||')
+
+calculateTotalProduct = productValue / 10
+
+print('----------------------------------')
+print('Valor do produto: R$', productValue)
+print('----------------------------------')
+print('Você teve um desconto de R$', calculateTotalProduct)
+print(calculateTotalProduct)
+print('----------------------------------')
